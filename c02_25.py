f434e1763a1cc3e014c7570e4481f0f57eb1605a
ourlifething <ryuji.hayama@gmail.com>
2023-08-05 10:54:24 +0900

    辞書のなかにセットをネスト

---
+++ c02_25.py
@@ -0,0 +1,13 @@
+member_hobbies = {
+	'松田':{'SNS', '麻雀', '自転車'},
+	'浅木':{'麻雀', '食べ歩き', '数学', '数学', '数学'}
+	}
+print(f'menber_hobbies{member_hobbies}')
+print(f'松田の趣味：{member_hobbies["松田"]}')
+print(f'浅木の趣味：{member_hobbies["浅木"]}')
+
+"""
+menber_hobbies{'松田': {'麻雀', 'SNS', '自転車'}, '浅木': {'麻雀', '食べ歩き', '数学'}}
+松田の趣味：{'麻雀', 'SNS', '自転車'}
+浅木の趣味：{'麻雀', '食べ歩き', '数学'}
+"""
