069aa751857c39ec3a3d8ca63823d09febc9698f
ourlifething <ryuji.hayama@gmail.com>
2023-08-07 10:14:13 +0900

    答えが分岐するチャットボット

---
+++ c03_02.py
@@ -0,0 +1,19 @@
+name = input('あなたの名前を教えてください>>')
+print(f'{name}さんこんにちは')
+food = input(f'{name}さんの好きな食べ物を教えてください>>')
+
+if food == 'カレー':
+		print('ステキですカレーは最高ですよね！！')
+else:
+		print(f'私も{food}が好きですよ')
+
+
+"""
+java
+if(food == "カレー"){
+
+}else{
+
+}
+"""
+
