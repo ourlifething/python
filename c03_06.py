92614215563eeef17c3ac265ffd08c4db0a9e8d9
ourlifething <ryuji.hayama@gmail.com>
2023-08-07 12:38:45 +0900

    ディクショナリのkeyをチェックする

---
+++ c03_06.py
@@ -0,0 +1,9 @@
+scores = {'network':60, 'database':80, 'security':50}
+
+key = input('追加する科目を入力してください>>')
+if key in scores:
+		print('既に登録済みです')
+else:
+		data = int(input('得点を入力して下さい>>'))
+		scores[key] = data
+print(scores)
