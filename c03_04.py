2b0cba0eb69192faf84b72ea054a757ff9447d82
ourlifething <ryuji.hayama@gmail.com>
2023-08-07 10:44:50 +0900

    in演算子

---
+++ c03_04.py
@@ -0,0 +1,11 @@
+name = input('あなたの名前を教えてください>>')
+print(f'{name}さんこんにちは')
+food = input(f'{name}さんの好きな食べ物を教えてください>>')
+
+if 'カレー' in food:
+		print('ステキですカレーは最高ですよね！！')
+else:
+		print(f'私も{food}が好きですよ')
+
+
+
