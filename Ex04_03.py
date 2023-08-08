16050fb43ef37fd37ec037773f2ee0eebc919911
ourlifething <ryuji.hayama@gmail.com>
2023-08-08 11:11:30 +0900

    カウンドダウン

---
+++ Ex04_03.py
@@ -0,0 +1,21 @@
+num = 10
+for i in range(10):
+	print(num - i , end =',')
+print('lift off!')
+
+for i in range(10,0,-1):
+	print(f'{i}', end=',')
+print('lift off!')
+
+"""
+10,9,8,7,6,5,4,3,2,1,lift off!
+10,9,8,7,6,5,4,3,2,1,lift off!
+"""
+
+for i in range(3,10,2):
+	print(i,end=',')
+print('lift off')
+
+#3,5,7,9,lift off
+
+#（１、１０）開始は含む終了は含まない
