54d83f1e1f21d8d9e35ffab94578558889010171
ourlifething <ryuji.hayama@gmail.com>
2023-08-09 20:42:50 +0900

    if~in演算値

--- c03_04.py
+++ c03_04.py
@@ -3,9 +3,9 @@ print(f'{name}さんこんにちは')
 food = input(f'{name}さんの好きな食べ物を教えてください>>')
 
 if 'カレー' in food:
-		print('ステキですカレーは最高ですよね！！')
+	print('ステキですカレーは最高ですよね！！')
 else:
-		print(f'私も{food}が好きですよ')
+	print(f'私も{food}が好きですよ')
 
 
 
