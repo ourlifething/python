a1572b6fbd1d52491287e5512481769016002dc7
ourlifething <ryuji.hayama@gmail.com>
2023-08-09 20:54:57 +0900

    1~6の乱数比較

--- diceEx.py
+++ diceEx.py
@@ -11,9 +11,9 @@ count +=1
 print(f'{count}回目{num2}')
 maxnum = max(num1,num2)
 if num1 == num2:
-		print('同じ')
+	print('同じ')
 elif maxnum == num1:
-		print(f'2回目は1回目よりも{num1-num2}小さいです')
+	print(f'2回目は1回目よりも{num1-num2}小さいです')
 else:
-		print(f'1回目は2回目よりも{num2-num1}小さいです')
+	print(f'1回目は2回目よりも{num2-num1}小さいです')
 
