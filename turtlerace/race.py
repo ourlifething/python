579c91fa3126c38c515603832c6d6c6e725bed0f
ourlifething <ryuji.hayama@gmail.com>
2023-08-09 21:14:05 +0900

    import turtle

--- turtlerace/race.py
+++ turtlerace/race.py
@@ -16,30 +16,30 @@ def setup():
 	startline = -620
 
 	for i in range(len(ts)):
-			t = turtle.Turtle()#亀インスタンス生成
-			t.shape('turtle')
-			t.color(ts[i])#亀の色設定
-			t.penup()#最初は線の描画はしない
-			t.setpos(startline,(len(ts)//2)* -40 + 40 *i)#亀のスタート位置
-			t.pendown()
-			ts[i] = t #リストを更新
+		t = turtle.Turtle()#亀インスタンス生成
+		t.shape('turtle')
+		t.color(ts[i])#亀の色設定
+		t.penup()#最初は線の描画はしない
+		t.setpos(startline,(len(ts)//2)* -40 + 40 *i)#亀のスタート位置
+		t.pendown()
+		ts[i] = t #リストを更新
 
 def race():
 	global ts
 	finishline = 540 #初期値540
 
 	while True:
-			for current_t in ts:
-					move = random.randint(0, 500)#初期値 0,20
-					current_t.forward(move)
+		for current_t in ts:
+			move = random.randint(0, 500)#初期値 0,20
+			current_t.forward(move)
 
-					x = current_t.xcor() #亀のx座標取得
-					#ゴールしたかの判定
-					if x >= finishline:
-							winner_color = current_t.color()#色取得
-							#文字の描画(内容,font(フォントの種類,文字の大きさ、ウェイト))
-							current_t.write('Win!'+ winner_color[0], font=('Arial', 16, 'bold'))
-							return #誰かがゴールしたら関数を抜ける
+			x = current_t.xcor() #亀のx座標取得
+			#ゴールしたかの判定
+			if x >= finishline:
+				winner_color = current_t.color()#色取得
+				#文字の描画(内容,font(フォントの種類,文字の大きさ、ウェイト))
+				current_t.write('Win!'+ winner_color[0], font=('Arial', 16, 'bold'))
+				return #誰かがゴールしたら関数を抜ける
 
 
 setup()
