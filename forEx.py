6a8aeec0e466f2dbf4d499dbc1a9139fc9fb24cb
ourlifething <ryuji.hayama@gmail.com>
2023-08-10 10:56:50 +0900

    range() for~ end=''

---
+++ forEx.py
@@ -0,0 +1,15 @@
+for i in range(5):
+	print(i)
+
+"""
+range()メソッド
+range(5) # 0,1,2,3,4
+range(3,5) # 3,4
+range(1,10,3) # 1,4,7
+"""
+
+#1から10まで以下のように出力せよ。
+#1 2 3 4 5 6 7 8 9 10
+for i in range(1,11):
+	print(i, end=' ')
+print()
