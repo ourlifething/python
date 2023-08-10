820d4f443d79a76c6b9bf0e8b68962d89dac2c3f
ourlifething <ryuji.hayama@gmail.com>
2023-08-10 10:37:25 +0900

    BMI算出、小数点切操作 :.2f

--- c01_23.py
+++ c01_23.py
@@ -1,3 +1,10 @@
+x = 10
+x = x + 100
+x = x / 3
+print(f'{x:.2f}')  # 36.67
+print(type(x))     # <class 'float'>
+# print(x:.2f)   error
+
 height = float(input('身長cm>>'))
 weight = float(input('体重kg>>'))
 bmi = weight / (height/100 * height / 100)
