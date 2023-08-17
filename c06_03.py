ce5fe78b620f105ecf8f38b1f900fd264d2c0879
ourlifething <ryuji.hayama@gmail.com>
2023-08-17 10:52:39 +0900

    class コンストラクター

--- c06_03.py
+++ c06_03.py
@@ -1,6 +1,6 @@
 userinfo = input('名前と血液型をカンマで区切って１行で入力>>')
 [name, blood] = userinfo.split(',')
-blood = blood.upper().strip()
+blood = blood.upper().strip()  # メソッドチェーン
 print(f'{name}さんは{blood}型なので大吉です')
 """
 strip():文字列の前後の空白を取り除く
---
+++ classLesson02.py
@@ -0,0 +1,24 @@
+class GameCharacter:
+	def __init__(self,job,life):
+		self.job = job
+		self.life = life
+	
+	def info(self):
+		print(self.job)
+		print(self.life)
+
+warrior = GameCharacter('戦士','100')
+warrior.info()
+
+"""
+戦士
+100
+"""
+
+wizzard = GameCharacter('魔法使い',80)
+wizzard.info()
+
+"""
+魔法使い
+80
+"""
