71329be0e1ea99ec2046e8225a7b10068fd327dc
ourlifething <ryuji.hayama@gmail.com>
2023-08-17 10:15:41 +0900

    split(),upper(),strip()

---
+++ c06_03.py
@@ -0,0 +1,8 @@
+userinfo = input('名前と血液型をカンマで区切って１行で入力>>')
+[name, blood] = userinfo.split(',')
+blood = blood.upper().strip()
+print(f'{name}さんは{blood}型なので大吉です')
+"""
+strip():文字列の前後の空白を取り除く
+upper():すべて大文字に
+"""
