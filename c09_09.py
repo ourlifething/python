3f49b3fdeb559f331b701be0127eb52ac6836e65
ourlifething <ryuji.hayama@gmail.com>
2023-08-23 10:38:04 +0900

    TypeError: unsupported operand type(s) for /: 'str' and 'int'

---
+++ c09_09.py
@@ -0,0 +1,3 @@
+x = 10
+print('hello' + x)
+#TypeError: can only concatenate str (not "int") to str
---
+++ c09_10.py
@@ -0,0 +1,2 @@
+print('hello' /5)
+# TypeError: unsupported operand type(s) for /: 'str' and 'int'
