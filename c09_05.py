99ed22b332b6d298dcaa6b3f6d7b4171e24fb9d8
ourlifething <ryuji.hayama@gmail.com>
2023-08-23 10:30:28 +0900

    File /Users/user/doc/python/c09_05.py, line 2

---
+++ c09_05.py
@@ -0,0 +1,12 @@
+x = 10
+if (x < 10)　:
+	print('hoge')
+else:
+print('foo')
+
+"""
+File "/Users/user/doc/python/c09_05.py", line 2
+    if (x < 10)　:
+               ^
+SyntaxError: invalid non-printable character U+3000
+"""
